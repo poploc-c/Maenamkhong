57afe9a6bb29730a51290562e89d10c15f8174cf
poploc-c <kitsananpop@gmail.com>
2026-06-13 15:50:07 +1200

    Wire FAQ component into page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import FullMenu from "./components/FullMenu";
 import Catering from "./components/Catering";
 import Reviews from "./components/Reviews";
 import Careers from "./components/Careers";
+import FAQ from "./components/FAQ";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import FlexEffects from "./components/FlexEffects";
@@ -32,13 +33,10 @@ export default function Home() {
         <MarqueeStrip variant="dark" />
         <Careers />
         <MarqueeStrip variant="light" />
+        <FAQ />
         <Contact />
       </main>
       <Footer />
     </div>
   );
 }
-
-
-
-
