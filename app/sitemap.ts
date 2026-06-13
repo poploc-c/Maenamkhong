a2a568342d2695bcb10c9b460d09dba5ec2077e7
poploc-c <kitsananpop@gmail.com>
2026-06-13 14:38:39 +1200

    Add sitemap.ts for Google Search Console

---
+++ app/sitemap.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+      {
+            url: "https://maenamkhong.com",
+                  lastModified: new Date(),
+                        changeFrequency: "weekly",
+                              priority: 1,
+                                  },
+                                    ];
+                                    }
+                                    
